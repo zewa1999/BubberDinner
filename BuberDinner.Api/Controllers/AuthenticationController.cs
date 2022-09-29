bfb97eef37a2ca0faca37a84576b43c5413c9753
zewa1999 <costacheandrei43@gmail.com>
2022-09-29 16:48:56 +0300

    added project

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
+    }
+}
