a81b40f1c02423cb610aa3f42dd94a49aa4a7e86
zewa1999 <costacheandrei43@gmail.com>
2022-10-03 16:25:51 +0300

    Addded Swagger bearer authentication

--- BuberDinner.Api/DependencyInjection.cs
+++ BuberDinner.Api/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using BuberDinner.Api.Common.Errors;
 using BuberDinner.Api.Common.Mapping;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 namespace BuberDinner.Api;
 
@@ -8,9 +9,52 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.fsadfgasd();
         services.AddControllers();
         services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
         services.AddMappings();
+        return services;
+    }
+    
+    public static IServiceCollection fsadfgasd(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Buber Dinner API",
+                Description = "An ASP.NET Core Web API where I learned more about Clean Architecture," +
+                              " CQRS, interesting design patterns and code",
+                Contact = new OpenApiContact
+                {
+                    Name = "Andrei Costache",
+                    Url = new Uri("https://www.linkedin.com/in/stelian-andrei-costache/")
+                }
+            });
+            
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.",
+                
+            });
+            
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme {
+                        Reference = new OpenApiReference {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
         return services;
     }
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,35 +1,16 @@
 using BuberDinner.Api;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddPresentation()
     .AddApplication()
     .AddAInfrastructure(builder.Configuration);
 }
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Buber Dinner API",
-        Description = "An ASP.NET Core Web API where I learned more about Clean Architecture," +
-        " CQRS, interesting design patterns and code",
-        Contact = new OpenApiContact
-        {
-            Name = "Andrei Costache",
-            Url = new Uri("https://www.linkedin.com/in/stelian-andrei-costache/")
-        }
-    });
-});
 
 var app = builder.Build();
 {
